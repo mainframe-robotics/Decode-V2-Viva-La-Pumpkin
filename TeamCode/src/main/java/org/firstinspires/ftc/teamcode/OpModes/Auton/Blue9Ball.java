f943f84b996c512e61dc19b7c9f55b055d8ec10d
Rishiramen <Rishiram.bandlamudi@github.com>
2025-12-13 02:05:06 -0600

    good luck kushal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auton/Blue9Ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auton/Blue9Ball.java
@@ -9,11 +9,24 @@ import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Control.Subsystems.Intake;
+import org.firstinspires.ftc.teamcode.Control.Subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.Control.Subsystems.Transfer;
+import org.firstinspires.ftc.teamcode.Control.Subsystems.Turret;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import java.util.Arrays;
 
 @Autonomous(name = "Example Auto", group = "Examples")
 public class Blue9Ball extends OpMode {
+    private Transfer transfer;
+    private Turret turret;
+    private Shooter shooter;
+
+    private Intake intake;
+
 
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
@@ -94,7 +107,28 @@ public class Blue9Ball extends OpMode {
 
                     /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
 //                    follower.followPath(scorePreload,true);
-                    follower.turnTo(Math.toRadians(180));
+                    follower.followPath(
+                            follower.pathBuilder()
+                                    .addPath(
+                                            new BezierLine(follower.getPose(), scorePreload.endPose())
+                                    )
+                                    .setConstantHeadingInterpolation(Math.toRadians(180))
+                                    .build(),
+                            true
+                    );
+
+                    setPathState(20);
+                }
+                break;
+            case 20:
+                if(!follower.isBusy()){
+                    state1=1;
+                    setPathState(21);
+                }
+
+                break;
+            case 21:
+                if(state1==-1){
                     setPathState(2);
                 }
                 break;
@@ -102,7 +136,8 @@ public class Blue9Ball extends OpMode {
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position */
                 if(!follower.isBusy()&&pathTimer.getElapsedTimeSeconds() > 1) {
                     /* Grab Sample */
-
+                    transfer.setTargetDeg(30,opmodeTimer.getElapsedTimeSeconds());
+                    intake.setPower(1);
                     /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     follower.followPath(intakeSet1,true);
                     setPathState(3);
@@ -110,12 +145,14 @@ public class Blue9Ball extends OpMode {
                 break;
             case 3:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
-                if(!follower.isBusy() &&pathTimer.getElapsedTimeSeconds() > 1) {
+                if(!follower.isBusy() &&pathTimer.getElapsedTimeSeconds() > 2) {
                     /* Score Sample */
+                    intake.setPower(0);
+                    transfer.setTargetDeg(240,opmodeTimer.getElapsedTimeSeconds());
 
                     /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(scoreSet1,true);
-                    setPathState(4);
+                    setPathState(-4);
                 }
                 break;
             case 4:
@@ -158,6 +195,7 @@ public class Blue9Ball extends OpMode {
         }
     }
 
+
     /** These change the states of the paths and actions. It will also reset the timers of the individual switches **/
     public void setPathState(int pState) {
         pathState = pState;
@@ -165,30 +203,184 @@ public class Blue9Ball extends OpMode {
     }
     @Override
     public void loop() {
+        double sec  = opmodeTimer.getElapsedTimeSeconds();
 
         // These loop the movements of the robot, these must be called continuously in order to work
         follower.update();
         autonomousPathUpdate();
+        turret.facePoint(goalPose,follower.getPose());
+        double goalDistance = Math.hypot(goalPose.getX()-follower.getPose().getX(),goalPose.getY()-follower.getPose().getY());
+
+        shooter.on();
+        if(state1!=-1) {
+            shooter.forDistance(goalDistance);
+        }else{
+            shooter.setTarget(0);
+        }
+        scoreBall(sec);
+        transfer.update(sec);
+        turret.update();
+        shooter.update();
+
 
         // Feedback to Driver Hub for debugging
         telemetry.addData("path state", pathState);
         telemetry.addData("x", follower.getPose().getX());
         telemetry.addData("y", follower.getPose().getY());
         telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("state1: ", state1);
+        telemetry.addData("cycles: ", cycles);
+        telemetry.addData("input: ", inputCounter);
+        telemetry.addData("shoot order: ", Arrays.toString(motif));
+        telemetry.addData("test: ", test);
         telemetry.update();
     }
+    int state1=-1;
+    int cycles=0;
+    ElapsedTime stateTimer;
+    int inputCounter=0;
+
+    String[] motif = {"PURPLE","GREEN","PURPLE"};
+    boolean test = false;
+
+    public void scoreBall(double sec){
+
+//        transfer.spinTo(x,sec,true);
+        switch (state1){
+            case 0:
+                if(inputCounter<3){
+                    if(gamepad1.rightBumperWasPressed()){
+                        motif[inputCounter]="PURPLE";
+                        inputCounter++;
+                    } else if (gamepad1.leftBumperWasPressed()) {
+                        motif[inputCounter]="GREEN";
+                        inputCounter++;
+                    }
+                }
+                else{
+                    state1=1;
+                }
+                break;
+            case 1:
+                transfer.scan(sec);
+                cycles=transfer.getNumBalls();
+                state1=2;
+                break;
+            case 2:
+                if(cycles>0){
+                    transfer.setTargetDeg(transfer.spinTo(motif[0],sec,true),sec);
+                    stateTimer.reset();
+                    state1=10;
+                }
+                else{
+                    state1=-1;
+                }
+                break;
+            case 10:
+                if (transfer.atTarget() && shooter.atTarget() && turret.atTarget()){
+                    transfer.score();
+                    stateTimer.reset();
+//                    test=true;
+                    state1=3;
+                }
+                break;
+            case 3:
+                if (stateTimer.milliseconds()>kickDown){
+                    if(cycles>1){
+                        transfer.scan(sec);
+                        transfer.setTargetDeg(transfer.spinTo(motif[1],sec,true),sec);
+                        state1=20;
+                    }
+                    else {
+                        state1=-1;
+                    }
+                }
+                else if (stateTimer.milliseconds()>kickerStall){
+                    transfer.retract();
+                }
+                break;
+            case 20:
+                if (transfer.atTarget()&& shooter.atTarget()&& turret.atTarget()){
+                    transfer.score();
+                    stateTimer.reset();
+                    state1=4;
+                }
+                break;
+
+            case 4:
+                if (stateTimer.milliseconds()>kickDown){
+                    if(cycles>2){
+                        transfer.scan(sec);
+                        transfer.setTargetDeg(transfer.spinTo(motif[2],sec,true),sec);
+                        state1=5;
+                    }
+                    else {
+                        state1=-1;
+                    }
+                }
+                else if (stateTimer.milliseconds()>kickerStall){
+                    transfer.retract();
+                }
+                break;
+            case 5:
+                if (transfer.atTarget()&& shooter.atTarget()&& turret.atTarget()){
+                    transfer.score();
+                    stateTimer.reset();
+                    state1=6;
+                }
+                break;
+            case 6:
+                if (stateTimer.milliseconds()>2000){
+                    transfer.retract();
+                    state1=-1;
+                }
+                break;
+
+
+//            case 4:
+//                if (stateTimer.milliseconds()>kickDown){
+//                    if(cycles>2){
+//                        transfer.scan(sec);
+//                        transfer.setTargetDeg(transfer.spinTo(motif[2],sec,true),sec);
+//                        if (transfer.atTarget()&& shooter.atTarget()&& turret.atTarget()){
+//                            transfer.score();
+//                            stateTimer.reset();
+//                            state1=-1;
+//                        }
+//                    }
+//                    else {
+//                        state1=-1;
+//                    }
+//                }
+//                else if (stateTimer.milliseconds()>kickerStall){
+//                    transfer.retract();
+//                }
+//                break;
+//            case 5:
+//
+
+        }
+    }
+    public static int kickerStall=700,kickDown=1300;
+
 
     /** This method is called once at the init of the OpMode. **/
     @Override
     public void init() {
         pathTimer = new Timer();
         opmodeTimer = new Timer();
+        stateTimer=new ElapsedTime();
         opmodeTimer.resetTimer();
 
+        transfer = new Transfer(hardwareMap);
+        turret = new Turret(hardwareMap);
+        shooter = new Shooter(hardwareMap);
+        intake = new Intake(hardwareMap);
 
         follower = Constants.createFollower(hardwareMap);
         buildPaths();
         follower.setStartingPose(startPose);
+
 
     }
 
